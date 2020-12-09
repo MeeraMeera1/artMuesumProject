0f4b447fc58bdf7a77c7b582bad4df17feefb951
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-08 19:24:33 -0500

    Set up 404 route and wrapped routes in Switch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation/index";
 import GalleryView from "./components/GalleryView/index";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="page-wrapper">
       <BrowserRouter>
         <GalleryNavigation galleries={harvardArt.records} />
-        <Route path="/galleries/:galleryId">
-          <GalleryView galleries={harvardArt.records} />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <h2>Harvard Art Museum</h2>
+            <p>
+              LOOK, but DON'T TOUCH. Please select a Gallery in the navigation
+              bar.
+            </p>
+          </Route>
+          <Route path="/galleries/:galleryId">
+            <GalleryView galleries={harvardArt.records} />
+          </Route>
+          <Route>
+            <h2>Page Not Found</h2>
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
